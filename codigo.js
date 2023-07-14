0bb72bac2781cf524f2f2caf19d5b1698ed93bfe
Reyes <Carlosreyes1803@gmail.com>
2023-07-14 11:32:37 -0600

    Creacion de la consulta php

---
+++ codigo.js
@@ -0,0 +1,60 @@
+const formulario = document.querySelector('form');
+const button = document.getElementById('buttonConsulta');
+
+const consultarPokemon = async (e) => {
+    e.preventDefault();
+    let nombrePokemon = formulario.pokemon.value;
+    
+    if (nombrePokemon === '') {
+        alert('Debe ingresar el nombre del Pokémon');
+        return;
+    }
+    
+    const url = `https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`;
+    const config = {
+        method: 'GET'
+    };
+
+    try {
+        const respuesta = await fetch(url, config);
+        
+        if (respuesta.status === 200) {
+            const data = await respuesta.json();
+            console.log(data.name);
+            console.log(data.weight);
+            console.log(data.types[0].type.name);
+            console.log(data.sprites.front_default);
+
+            document.getElementById('nombrePokemon').innerText = data.name;
+            document.getElementById('pesoPokemon').innerText = data.weight;
+            document.getElementById('tipoPokemon').innerText = data.types[0].type.name;
+            document.getElementById('imagenPokemon').src = data.sprites.front_default;
+            document.getElementById('estado').innerText = 'Pokemon encontrado';
+        } else {
+            alert('Error en la consulta');
+        }
+        
+        console.log(respuesta);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const consultaAPI = async () => {
+    const url = '/Clase 4 progra/codigo.php';
+    const config = {
+        method: 'GET'
+    };
+
+    try {
+        const respuesta = await fetch(url, config);
+        const data = await respuesta.text();
+
+        alert(data);
+    } catch (error) {
+        alert(error);
+    }
+}
+
+formulario.addEventListener('submit', consultarPokemon);
+button.addEventListener('click', consultaAPI);
